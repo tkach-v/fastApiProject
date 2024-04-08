0efb8af7b0f9ddf62a4f7d390502f412dc5dbe9c
VolskiyRoman <romanvolskiy791@gmail.com>
2024-04-08 21:23:02 +0300

    fixed CORS

--- app/main.py
+++ app/main.py
@@ -10,11 +10,15 @@ from app.core.config import settings
 
 app = FastAPI(title="Repair Workshop")
 
+origins = [
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
